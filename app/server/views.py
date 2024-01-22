7d1f6655a77ccae0d171817882b921c0cce07647
bjklayneabordofsd <bjklayneabordo.fsd@gmail.com>
2024-01-23 01:47:06 +0800

    Created an API Endpoint for Filtering Servers by Quantity

--- app/server/views.py
+++ app/server/views.py
@@ -12,9 +12,14 @@ class ServerListViewSet(viewsets.ViewSet):
     def list (self, request):
         """category from ServerModel"""
         category = request.query_params.get("category")
+        qty = request.query_params.get("qty")
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
+
+        """Quantity of servers returned"""
+        if qty:
+            self.queryset = self.queryset[: int(qty)]
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
