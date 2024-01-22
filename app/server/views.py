9b330a93f6ef2421213fb7cffd77b8dc9fdb2b7d
bjklayneabordofsd <bjklayneabordo.fsd@gmail.com>
2024-01-23 02:02:03 +0800

    Created an API Endpoint for Filtering Servers by User

--- app/server/views.py
+++ app/server/views.py
@@ -13,13 +13,20 @@ class ServerListViewSet(viewsets.ViewSet):
         """category from ServerModel"""
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
+        by_user = request.query_params.get("by_user") == 'true'
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
+        
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)
 
         """Quantity of servers returned"""
         if qty:
             self.queryset = self.queryset[: int(qty)]
+
+
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
