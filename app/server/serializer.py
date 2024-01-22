a043a9034fad93130b059d4a5e1f63a0ca953518
bjklayneabordofsd <bjklayneabordo.fsd@gmail.com>
2024-01-23 02:49:27 +0800

    Built: Returning Related Data - Server Associated Channels

--- app/server/serializer.py
+++ app/server/serializer.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 
-from .models import CategoryModel, ServerModel
+from .models import CategoryModel, ServerModel, ChannelModel
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChannelModel
+        fields = "__all__"
 
 class ServerSerializer(serializers.ModelSerializer):
+    channel_server = ChannelSerializer(many=True)
 
     class Meta:
         model = ServerModel
