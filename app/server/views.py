1023b64ff2a733913dce85b6492d6c88a710240a
bjklayneabordofsd <bjklayneabordo.fsd@gmail.com>
2024-01-24 02:49:03 +0800

    Created an API Endpoint for Filtering Servers and Returning Annotation

--- app/server/views.py
+++ app/server/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Count
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
@@ -17,6 +18,7 @@ class ServerListViewSet(viewsets.ViewSet):
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == 'true'
         by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == 'true'
 
         if by_user and not request.user.is_authenticated:
             raise AuthenticationFailed()
@@ -27,6 +29,9 @@ class ServerListViewSet(viewsets.ViewSet):
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
 
         """Quantity of servers returned"""
         if qty:
@@ -40,5 +45,5 @@ class ServerListViewSet(viewsets.ViewSet):
             except ValueError:
                 raise ValueError(detail=f"Server value error")
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
         return Response(serializer.data)
