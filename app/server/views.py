e8dcf69d05639e9326ee6ee6e4a857f0a8db7f5b
bjklayneabordofsd <bjklayneabordo.fsd@gmail.com>
2024-01-24 23:04:20 +0800

    Created an API Endpoint Decorator for Detailing Endpoints

--- app/server/views.py
+++ app/server/views.py
@@ -3,11 +3,11 @@ from django.db.models import Count
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
-
+from .schema import server_list_docs
 from .serializer import ServerSerializer
 from .models import ServerModel
 
-
+@server_list_docs
 class ServerListViewSet(viewsets.ViewSet):
 
     queryset = ServerModel.objects.all()
@@ -20,15 +20,18 @@ class ServerListViewSet(viewsets.ViewSet):
         by_serverid = request.query_params.get("by_serverid")
         with_num_members = request.query_params.get("with_num_members") == 'true'
 
-        if by_user and not request.user.is_authenticated:
-            raise AuthenticationFailed()
+        #if by_user and not request.user.is_authenticated:
+            #raise AuthenticationFailed()
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
         
         if by_user:
-            user_id = request.user.id
-            self.queryset = self.queryset.filter(member=user_id)
+            if by_user and request.user.is_authenticated:
+                user_id = request.user.id
+                self.queryset = self.queryset.filter(member=user_id)
+            else:
+                raise AuthenticationFailed()
 
         if with_num_members:
             self.queryset = self.queryset.annotate(num_members=Count("member"))
@@ -38,6 +41,9 @@ class ServerListViewSet(viewsets.ViewSet):
             self.queryset = self.queryset[: int(qty)]
 
         if by_serverid:
+            if not request.user.is_authenticated:
+                raise AuthenticationFailed()
+            
             try:
                 self.queryset = self.queryset.filter(id=by_serverid)
                 if not self.queryset.exists():
