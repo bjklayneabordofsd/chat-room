b3c63e9f5865a1cf19863e40d51fbd01f2a3f5e2
bjklayneabordofsd <bjklayneabordo.fsd@gmail.com>
2024-01-28 19:16:11 +0800

    Model Refactor for Icons and Banners in Django ServerModel

--- app/server/models.py
+++ app/server/models.py
@@ -4,6 +4,12 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
 
+def server_icon_upload_path(instance, filename):
+    return f"server/{instance.id}/server_icon/{filename}"
+
+def server_banner_upload_path(instance, filename):
+    return f"server/{instance.id}/server_banner/{filename}"
+
 def category_icon_upload_path(instance, filename):
     return f"category/{instance.id}/category_icon/{filename}"
 
@@ -49,10 +55,25 @@ class ChannelModel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(ServerModel, on_delete=models.CASCADE, related_name="channel_server")
+    banner = models.ImageField(upload_to=server_banner_upload_path, null=True, blank=True)
+    icon = models.ImageField(upload_to=server_icon_upload_path, null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()
-        super(ChannelModel, self).save(*args, **kwargs)
+        if self.id:
+            existing = get_object_or_404(ServerModel, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+        super(ServerModel, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, send="server.ServerModel")
+    def server_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon" or field.name == "banner":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
