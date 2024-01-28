b8d828c36c4f00a9ad322f6847a62803343f2192
bjklayneabordofsd <bjklayneabordo.fsd@gmail.com>
2024-01-28 18:28:42 +0800

    Model Refactor for Icons and Banners in Django CategoryModel

--- app/server/models.py
+++ app/server/models.py
@@ -1,10 +1,35 @@
 from collections.abc import Iterable
 from django.db import models
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django.dispatch import receiver
+
+def category_icon_upload_path(instance, filename):
+    return f"category/{instance.id}/category_icon/{filename}"
 
 class CategoryModel(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(
+        upload_to=category_icon_upload_path, 
+        null=True, 
+        blank=True,
+        )
+    
+    def save(self, *args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(CategoryModel, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        super(CategoryModel, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, send="server.CategoryModel")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
